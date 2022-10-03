4ae98e489c31568dccb1554c9ace54b2813e6e98
Z <NoMoreOppress@gmail.com>
2022-10-03 13:15:37 +0200

    Enter a message <Required>

---
+++ Project NSS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Project_NSS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
