36ffae5aeaf337707f54b28e65107db59596a208
Z <NoMoreOppress@gmail.com>
2022-10-05 14:21:12 +0200

    fixing stuff

---
+++ NordicDoorSuggestionSystem/Database/SuggestionDB.sql
@@ -0,0 +1,16 @@
+﻿create DATABASE if not EXISTS NSS;
+
+use NSS;
+
+CREATE TABLE if NOT EXISTS employee
+( EmplyeeNumber: int not null,
+  FirstName: varchar(50) not null,
+  LastName: varchar(50) not null,
+  Password: varchar(50) not null,
+  AccountPrivilege: tinyint not null,
+  AccountState: boolean not null,
+  SgstnCount: smallint,
+  ProfilePicture: varbinary,
+
+  Primary key (EmplyeeNumber)
+  );
