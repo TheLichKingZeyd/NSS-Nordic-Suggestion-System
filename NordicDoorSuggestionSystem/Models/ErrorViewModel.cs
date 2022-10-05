84a91417e99b4f3fdfeb65406051f6c7ecbede3e
Markus Fjelltun <89394241+ZumbaThirst@users.noreply.github.com>
2022-10-05 13:59:46 +0200

    Add files via upload

---
+++ NordicDoorSuggestionSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NordicDoorSuggestionSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
