6cce884c5a3cf6891cc1b42a703fab826265c749
Z <NoMoreOppress@gmail.com>
2022-10-06 12:19:43 +0200

    created a suggestions table

--- NordicDoorSuggestionSystem/Database/SuggestionDB.sql
+++ NordicDoorSuggestionSystem/Database/SuggestionDB.sql
@@ -1,9 +1,9 @@
-﻿create DATABASE if not EXISTS NSS;
+﻿CREATE DATABASE IF NOT EXISTS SuggestionDB;
 
-use NSS;
+use SuggestionDB;
 
-CREATE TABLE if NOT EXISTS employee
-( EmplyeeNumber: int not null,
+CREATE TABLE IF NOT EXISTS Employee
+( EmployeeNumber: int not null,
   FirstName: varchar(50) not null,
   LastName: varchar(50) not null,
   Password: varchar(50) not null,
@@ -12,5 +12,29 @@ CREATE TABLE if NOT EXISTS employee
   SgstnCount: smallint,
   ProfilePicture: varbinary,
 
-  Primary key (EmplyeeNumber)
+  Primary key (EmployeeNumber)
   );
+
+  CREATE TABLE IF NOT EXISTS Suggestion (
+
+  SgstnID: INT NOT NULL,
+  EmployeeNumber: INT NOT NULL,
+  TeamID: INT DEFAULT 0,
+  ResponsibleEmployee: INT NOT NULL,
+  SubmissionTime: DATETIME NOT NULL,
+  ETA: DATETIME NOT NULL,
+  Title: TINYTEXT,
+  ProbDescr: TEXT,
+  Solution: TEXT,
+  Goal: TEXT,
+  HasMediaAttachments: BIT NOT NULL,
+  Progress: TINYINT DEFAULT 0,
+  HasReasoning: BIT NOT NULL,
+
+  PRIMARY KEY (SgstnID),
+  FOREIGN KEY (EmployeeNumber) REFERENCES Employee(EmployeeNumber),
+  FOREIGN KEY (TeamID) REFERENCES Team(TeamID),
+  FOREIGN KEY (ResponsibleEmployee) REFERENCES Employee(EmployeeNumber),
+
+  );
+
