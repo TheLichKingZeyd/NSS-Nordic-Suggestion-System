2ad1587b2d7b1249dbaefde80d8c3afed6206797
uberskau <markus.skau@gmail.com>
2022-11-22 14:30:25 +0100

    added user info
    on profile

--- NordicDoorSuggestionSystem/Controllers/ProfilePageController.cs
+++ NordicDoorSuggestionSystem/Controllers/ProfilePageController.cs
@@ -3,6 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using NordicDoorSuggestionSystem.Entities;
+using NordicDoorSuggestionSystem.Repositories;
+using System.Data;
+using NordicDoorSuggestionSystem.DataAccess;
+using NordicDoorSuggestionSystem.Models;
+using NordicDoorSuggestionSystem.Models.Employees;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using NordicDoorSuggestionSystem.Models.Account;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,10 +21,35 @@ namespace NordicDoorSuggestionSystem.Controllers
 {
     public class ProfilePageController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
+        private readonly UserManager<User> _userManager;
+        private readonly DataContext _context;
+
+        public ProfilePageController(UserManager<User> userManager, DataContext context)
         {
-            return View();
+            _userManager = userManager;
+            _context = context;
+        }
+
+        // GET: /<controller>/
+        public async Task<IActionResult> Index()
+        {
+            var currentUser = await _userManager.GetUserAsync(HttpContext.User);
+            ProfileViewModel vm = new ProfileViewModel();
+            if (currentUser == null)
+            {
+                return NotFound();
+            }
+            vm.EmployeeNumber = currentUser.EmployeeNumber;
+            vm.FirstName = currentUser.FirstName;
+            vm.LastName = currentUser.LastName;
+            vm.Role = currentUser.Role;
+            var mittTeam = _context.Employees.Where(e => e.EmployeeNumber.Equals(vm.EmployeeNumber)).Select(e => e.TeamID).FirstOrDefault();
+            vm.TeamID = mittTeam;
+            var teamname = _context.Team.Where(e => e.TeamID.Equals(vm.TeamID)).Select(e => e.TeamName).FirstOrDefault();
+            vm.TeamName = teamname;
+            var mincount = _context.Employees.Where(e => e.EmployeeNumber.Equals(vm.EmployeeNumber)).Select(e => e.SuggestionCount).FirstOrDefault();
+            vm.SuggestionCount = mincount;
+            return View(vm);
         }
 
         // GET: /<controller>/
