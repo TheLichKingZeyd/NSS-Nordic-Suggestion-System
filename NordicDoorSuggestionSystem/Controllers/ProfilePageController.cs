5467f34e5d09de256c159ef9a8ef4c09327a9068
Z <NoMoreOppress@gmail.com>
2022-11-23 08:42:13 +0100

    saafa

--- NordicDoorSuggestionSystem/Controllers/ProfilePageController.cs
+++ NordicDoorSuggestionSystem/Controllers/ProfilePageController.cs
@@ -22,5 +22,46 @@ namespace NordicDoorSuggestionSystem.Controllers
             return View();
         }
     }
+
+
+/*
+    [HttpGet]
+    public async Task<IActionResult> UploadProfilePicture(byte[] picture)
+    {
+        var user = await _userManager.FindByIdAsync(id);
+        var currentUser = _userManager.GetUserAsync(HttpContext.User);
+        user.EmployeeNumber ;
+    }
+*/
+   /* public async Task<IActionResult> Edit(int? id)
+    {
+        if (id == null || await _suggestionRepository.GetSuggestions() == null)
+        {
+            return NotFound();
+        }
+
+        var suggestion = await _suggestionRepository.GetSuggestion(id);
+        if (suggestion == null)
+        {
+            return NotFound();
+        }
+
+        var editSuggestionViewModel = new EditSuggestionViewModel
+        {
+            Title = suggestion.Title,
+            ResponsibleEmployee = suggestion.ResponsibleEmployee,
+            Problem = suggestion.Problem,
+            Solution = suggestion.Solution,
+            Goal = suggestion.Goal,
+            Deadline = suggestion.Deadline,
+            TeamID = suggestion.TeamID.Value
+        };
+
+        return View(editSuggestionViewModel);
+    }*/
+
+
+
+
 }
 
